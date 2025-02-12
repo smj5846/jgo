2ca1f934bcb95f38e775fbc15e048c3273cd4248
smj5846 <smj5846@naver.com>
2025-02-12 12:10:21 +0900

    h2데이터베이스 및 jpa 연결

---
+++ src/test/java/com/mysite/jgo/JgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.jgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
