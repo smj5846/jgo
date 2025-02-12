853a36a0a80b7a785c56122f8ffd1156662b8fdd
smj5846 <smj5846@naver.com>
2025-02-12 11:48:30 +0900

    Add files via upload

---
+++ jgo/src/main/java/com/mysite/jgo/JgoApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.jgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgoApplication.class, args);
+	}
+
+}
